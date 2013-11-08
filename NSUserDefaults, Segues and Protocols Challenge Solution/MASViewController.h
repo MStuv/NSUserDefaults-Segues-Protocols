e150699fec022536af0c40fd4933de52c786a12d
MStuv <markstuver@MacBook-Pro.local>
2013-11-07 23:23:50 -0500

    Initial Commit

---
+++ NSUserDefaults, Segues and Protocols Challenge Solution/MASViewController.h
@@ -0,0 +1,13 @@
+//
+//  MASViewController.h
+//  NSUserDefaults, Segues and Protocols Challenge Solution
+//
+//  Created by Mark Stuver on 11/7/13.
+//  Copyright (c) 2013 Halo International Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MASViewController : UIViewController
+
+@end
